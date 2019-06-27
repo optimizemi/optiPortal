9e6519e1a93f4faa7fc6eb07903a47bb2c8bf70d
Bunnydreams <pcjh@umich.edu>
2019-06-27 10:22:03 +0800

    adds necessary javascript to get navbar burger working

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="https://optiportal.netlify.com">
-        <img src="../assets/logo.png" width="112">
+        <img src="../assets/logo.png" width="112" />
       </a>
 
       <a
@@ -44,8 +44,11 @@
             <a
               class="navbar-item"
               href="https://docs.google.com/document/d/18XfRsNlunHT1nVqBFkaxDZONdGGwtq4zYNixJieLgMg"
-            >Design Docs</a>
-            <a class="navbar-item" href="https://forms.gle/uUcQEsBAxhSpsZHFA">Bug/Feature Report</a>
+              >Design Docs</a
+            >
+            <a class="navbar-item" href="https://forms.gle/uUcQEsBAxhSpsZHFA"
+              >Bug/Feature Report</a
+            >
           </div>
         </div>
       </div>
@@ -65,8 +68,37 @@
 
 
 <script>
+function initializeNavbarBurger() {
+  document.addEventListener("DOMContentLoaded", () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(
+      document.querySelectorAll(".navbar-burger"),
+      0
+    );
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach(el => {
+        el.addEventListener("click", () => {
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle("is-active");
+          $target.classList.toggle("is-active");
+        });
+      });
+    }
+  });
+}
+
 export default {
-  name: "NavBar"
+  name: "NavBar",
+  mounted() {
+    initializeNavbarBurger();
+  }
 };
 </script>
 

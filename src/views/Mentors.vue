91f94817febfb3fb27f46d5533524df555d1508f
bunnydreams <pcjh@umich.edu>
2019-07-18 11:28:50 +0800

    Bug fix: fixes optiMize favicons
    Feature: New ProfileCard UI

--- src/views/Mentors.vue
+++ src/views/Mentors.vue
@@ -63,6 +63,7 @@ form {
 }
 .cardWrapper {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  grid-gap: 1.5rem;
 }
 </style>

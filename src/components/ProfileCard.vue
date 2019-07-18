c9f2e97abfcc014e9af726501f6eb560746e31dc
bunnydreams <pcjh@umich.edu>
2019-07-18 11:14:02 +0800

    Warning: disables amplify Authenticator
    Bug fix: updates Buefy color palette
    
    Feature: creates mock up of search bar, updates ProfileCard component to take in entire mentor object and renders accordingly

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="profile-card">
-    <h1>{{ name }}</h1>
-    <h2>Currently {{ currentRole}} at {{ currentEmployer }}</h2>
-    <p>{{schoolName}} class of {{ schoolGradYear }}</p>
-    <p>{{ bio }}</p>
-    <hr>
-    <h3>Contact Infor</h3>
-    <ul>
-      <li>Social media links</li>
-    </ul>
+    <img :src="mentor.photo" />
+    <h1>{{ mentor.name }}</h1>
+    <h3>{{ mentor.city }}, {{ mentor.state }}</h3>
+    <p>{{ mentor.bio }}</p>
+    <p>{{ mentor.skills }}</p>
+    <hr />
+    <h3>Contact Information</h3>
+    <p v-if="mentor.email">
+      Email: <a :href="'mailto:' + mentor.email">{{ mentor.email }} </a>
+    </p>
+    <p v-if="mentor.phone">Phone: {{ mentor.phone }}</p>
+    <p v-if="mentor.linkedin">
+      LinkedIn:
+      <a :href="mentor.linkedin" target="_blank">{{ mentor.linkedin }}</a>
+    </p>
   </div>
 </template>
 
@@ -16,27 +22,13 @@
 export default {
   name: "ProfileCard",
   props: {
-    name: String,
-    userType: Array,
-    currentRole: String,
-    currentEmployer: String,
-    schoolGradYear: Number,
-    schoolName: String,
-    location: String,
-    bio: String,
-    phone: String,
-    email: String,
-    project: Array,
-    skills: Array
+    mentor: Object
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;

1b5e1c65d0dd9837532ce523900f73730bc85326
Jake Beason <jpbeason@umich.edu>
2020-06-25 17:34:16 -0400

    Working mentors v-for prop pass

--- pages/mentors.vue
+++ pages/mentors.vue
@@ -11,7 +11,11 @@
     <hr />
 
     <div class="cardWrapper">
-      <ProfileCard v-for="mentor in data" :mentor="mentor" :key="mentor.name" />
+      <ProfileCard
+        v-for="mentor in mentors.body"
+        :key="mentor.mentorId"
+        :mentor="mentor"
+      />
     </div>
   </div>
 </template>
@@ -25,11 +29,36 @@ export default {
   components: { ProfileCard },
   data() {
     return {
-      mentors: null,
+      mentors: {
+        statusCode: 200,
+        body: [
+          {
+            mentorId: 81,
+            firstName: 'Aaron',
+            lastName: 'Stryker',
+            email: 'astryker@wesleyan.edu',
+            phone: '(734) 834-7604',
+            currentPosition: 'Director',
+            currentCity: 'Ann Arbor',
+            currentState: 'MI'
+          },
+          {
+            mentorId: 117,
+            firstName: 'Aashka',
+            lastName: 'Soni',
+            email: 'aashkahsoni@gmail.com',
+            phone: '(248) 496-7722',
+            currentPosition: 'Partner Development',
+            currentCity: 'Farmington Hills',
+            currentState: 'MI'
+          }
+        ]
+      },
       loading: true,
       errored: false
     }
-  },
+  }
+  /*
   mounted() {
     this.$axios
       .get(`https://p5mq2fkwwb.execute-api.us-east-2.amazonaws.com/v1/mentors`)
@@ -40,6 +69,7 @@ export default {
       })
       .finally(() => (this.loading = false))
   }
+  */
 }
 </script>
 
